d55876a360830c108f91633cb9d3ca684448048b
Darkidd77 <m.almahdi77@yahoo.com>
2023-10-08 12:17:00 -0700

    Hello wrld

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 (Success)
+ */
+int main (void)
+{
+	puts("\"Programing is like building a multilingual puzzle");
+	return(0);
+}
