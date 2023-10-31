3235a53e424fd61ddfbb11c70b05c828d1bba1b1
“Darkidd77” <m.almahdi77@yahoo.com>
2023-10-31 11:08:43 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - a function that print the program name 
+ * @argc: arguement count
+ * @argv: argument variable 
+ * Return: success 
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
