90e67ef027054563cb6a88e04a5fba799537820d
“Darkidd77” <m.almahdi77@yahoo.com>
2023-11-07 11:19:57 -0800

    Betty fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,13 +13,12 @@
     */
 
 
-   struct dog
+struct dog
 {
-   char *name;
-   float age;
-   char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
-
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
