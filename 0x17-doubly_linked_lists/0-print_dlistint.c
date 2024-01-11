a9dcd3f1c82970af916db530ed948755c233998e
“Darkidd77” <m.almahdi77@yahoo.com>
2024-01-11 20:00:26 +0000

    task-0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints every element
+ *
+ *
+ * @h: head of list
+ * Return: the number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int num;
+
+	num = 0;
+
+	if (h == NULL)
+	{
+		return (num);
+	}
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
+	}
+
+
+	while (h->prev != NULL)
+	{
+		h = h->prev;
+	}
+
+	return (num);
+}
